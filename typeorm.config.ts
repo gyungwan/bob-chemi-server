6e0074fc7c2a142cfcaa538049c9234d16883895
gyungwan <zddx1994@gmail.com>
2023-05-23 15:40:04 +0900

    [ FEATURE ] 도커,DB 연결

---
+++ typeorm.config.ts
@@ -0,0 +1,20 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'your_database',
+  synchronize: true,
+  logging: true,
+  entities: [__dirname + '/src/entities/*.entity{.ts,.js}'],
+  //   migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
+  //   subscribers: [__dirname + '/src/subscribers/*{.ts,.js}'],
+  //   cli: {
+  //     entitiesDir: 'src/entities',
+  //     migrationsDir: 'src/migrations',
+  //     subscribersDir: 'src/subscribers',
+  //   },
+};
