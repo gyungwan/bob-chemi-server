8768424ef79efeb1f4f0c167ba6120bd79e6ef5a
Yeji Choi <cyei0824@gmail.com>
2023-05-24 00:19:35 +0900

    [FEATURE] quickMatching 파일 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    // SwaggerModule.forRoot({
+    //   swaggerCustomOptions: {
+    //     swaggerOptions: {
+    //       // Swagger UI 옵션
+    //     },
+    //   },
+    //   // 다양한 설정 옵션
+    // }),
     TypeOrmModule.forRoot({
       type: process.env.DATABASE_TYPE as "postgres",
       host: process.env.DATABASE_HOST,
