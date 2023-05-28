8691867981b17804643e5162209ec75ed0463e13
HEisSHAM <114637197+HEisSHAM@users.noreply.github.com>
2023-05-28 16:07:47 +0900

    [ UPDATE ] 게시판 코드 수정

--- src/apis/group/boards/boards.service.ts
+++ src/apis/group/boards/boards.service.ts
@@ -1,48 +1,47 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
-import { Board, BoardStatus } from "./boards.model";
+import { BoardStatus } from "./boards.status.enum";
 import { v1 as uuid } from "uuid";
 import { CreateBoardDto } from "./dto/create.board.dto";
+import { BoardRepository } from "./board.repository";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Board } from "./entity/boards.entity";
 
 @Injectable()
 export class BoardService {
-  private boards: Board[] = [];
-
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository
+  ) {}
   //<<------------게시글 조회------------>>
-  getAllBoards(): Board[] {
-    return this.boards;
+  async getAllBoards(): Promise<Board[]> {
+    return this.boardRepository.find();
   }
-
   //<<------------ID로 게시글 조회------------>>
-  getBoardById(id: string): Board {
-    const found = this.boards.find((board) => board.id === id);
-    if (!found) throw new NotFoundException(`Cannot find Board with id: ${id}`);
+  async getBoardById(id: any): Promise<Board> {
+    const found = await this.boardRepository.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException(`Cannot find Board with ID ${id}`);
+    }
     return found;
   }
-
   //<<------------게시글 작성------------>>
-  createBoard(createBoardDto: CreateBoardDto) {
-    const { title, description } = createBoardDto;
-
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
-    this.boards.push(board);
-    return board;
+  createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardRepository.createBoard(createBoardDto);
   }
-
   //<<------------게시글 삭제------------>>
-  deleteBoard(id: string): void {
-    const found = this.getBoardById(id);
-    this.boards = this.boards.filter((board) => board.id !== found.id);
+  async deleteBoard(id: number): Promise<void> {
+    const result = await this.boardRepository.softDelete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Cannot find Board with id ${id}`);
+    }
   }
-
-  //<<------------게시글 상태 수정------------>>
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
+  //<<------------게시글 수정------------>>
+  async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+    const board = await this.getBoardById(id);
     board.status = status;
+    await this.boardRepository.save(board);
+
     return board;
   }
 }
