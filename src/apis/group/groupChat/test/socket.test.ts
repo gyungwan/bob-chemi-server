0692b08fd75d0dd16754d45b67a587c8a3aaf8d9
HEisSHAM <114637197+HEisSHAM@users.noreply.github.com>
2023-06-14 18:42:16 +0900

    [FIX]에러수정, [FEATURE]swagger tag 추가

---
+++ src/apis/group/groupChat/test/socket.test.ts
@@ -0,0 +1,21 @@
+// import * as io from "socket.io-client";
+
+// const socket = io("http://localhost:3000");
+
+// // 소켓 연결 시 이벤트
+// socket.on("connect", () => {
+//   console.log("Connected to server");
+// });
+
+// // 특정 이벤트 수신 시 동작
+// socket.on("customEvent", (data: any) => {
+//   console.log("Received data:", data);
+// });
+
+// // 서버에 데이터 전송
+// socket.emit("customEvent", { message: "Hello, server!" });
+
+// // 소켓 연결 해제 시 이벤트
+// socket.on("disconnect", () => {
+//   console.log("Disconnected from server");
+// });
